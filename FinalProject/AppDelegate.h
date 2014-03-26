7d172466d7bb8ed90cbc5dca0f6135d612aa40f7
cknutson442 <cknutson@smu.edu>
2014-03-26 16:23:49 -0500

    Initial Commit

---
+++ FinalProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FinalProject
+//
+//  Created by CONNER KNUTSON on 3/26/14.
+//  Copyright (c) 2014 CONNER KNUTSON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FinalProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FinalProject
+//
+//  Created by CONNER KNUTSON on 3/26/14.
+//  Copyright (c) 2014 CONNER KNUTSON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
