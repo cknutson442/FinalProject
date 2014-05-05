e580385e99d8c2f6412cb5cc31d07f0ae0d3206d
hhouston <hhouston@smu.edu>
2014-05-04 21:32:25 -0500

    a

--- FinalProject/GroupViewController.h
+++ FinalProject/GroupViewController.h
@@ -1,21 +1,18 @@
-//
-//  ViewController.h
-//  FinalProject
-//
-//  Created by CONNER KNUTSON on 3/26/14.
-//  Copyright (c) 2014 CONNER KNUTSON. All rights reserved.
-//
-
 #import <GLKit/GLKit.h>
 #import <UIKit/UIKit.h>
 #import <opencv2/opencv.hpp>
 
-@interface GroupViewController : UIViewController<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+@interface GroupViewController : UIViewController<UIImagePickerControllerDelegate,UINavigationControllerDelegate> {
+    double _min, _max;
+    __weak IBOutlet UISlider *_slider;
+    __weak IBOutlet UILabel *_labelValue;
+}
 
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) IBOutlet UIImageView *tempImageView;
 @property (strong, nonatomic) IBOutlet UILabel *objectSizeLabel;
 - (IBAction)pickImageFromGallery:(id)sender;
 - (IBAction)takePictureFromCamera:(id)sender;
+- (IBAction)sliderValueChanged:(id)sender;
 
 @end
--- FinalProject/GroupViewController.mm
+++ FinalProject/GroupViewController.mm
@@ -1,27 +1,40 @@
-//
-//  GroupViewController.m
-
-//
+#import <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#import "opencv2/opencv.hpp"
 
 #import "GroupViewController.h"
 
-//using namespace std;
-//using namespace cv;
+using namespace std;
+using namespace cv;
 
 @interface GroupViewController ()
 
 @end
 
 @implementation GroupViewController
-
+UIImageOrientation orientation;
 cv::Mat imageMat;
+cv::Mat imageMat2;
+
 cv::vector<cv::Mat> BGR;
 CGPoint lastPoint = {-1,-1};
 CGPoint minPoint = {9999,9999}, maxPoint = {-1,-1};
 bool mouseSwiped;
 
+IplImage* iplImage;
+UIImage* image2;
+
+
+//Circle::Circle(cv::Mat imageMat,Point const& width, Point const& height);
+
 - (void)viewDidLoad{
     [super viewDidLoad];
+    
+
+    
+    _labelValue.text = [NSString stringWithFormat:@"%.2f - %.2f", _min, _max];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -32,15 +45,55 @@ bool mouseSwiped;
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo{
     [picker dismissModalViewControllerAnimated:YES];
-    imageMat = [self cvMatFromUIImage:image];
-    cv::cvtColor(imageMat, imageMat, CV_RGB2GRAY);
     
-    //the line detection process is here
-    cv::Canny(imageMat, imageMat, 35, 90);
+//    medianBlur(imageMat,imageMat,5);
+//
+//    imageMat = [self cvMatFromUIImage:image];
+//    //    cvtColor(img, cimg, COLOR_GRAY2BGR);
+//    cvtColor(imageMat, imageMat2, CV_RGB2GRAY);
+//    
+//    
+//    //split(imageMat,BGR);
+//    
+//    cv::vector<cv::Vec3f>circles;
+//    HoughCircles(imageMat2, circles, CV_HOUGH_GRADIENT, 1, 10,
+//                 100, 30, 1, 30
+//                 
+//                 );
+//    
+//    for(size_t i = 0; i < circles.size(); i++)
+//    {
+////        Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+//        int radius = cvRound(circles[i][2]);
+////        // circle center
+////        circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
+////        // circle outline
+////        circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
+////        cv::Vec3i c = circles[i];
+////        circle( imageMat2, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, LINE_AA);
+////        circle( imageMat2, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, LINE_AA);
+//        
+//        NSLog(@"%d",radius);
+//        
+//    }
     
-    split(imageMat,BGR);
+    //_imageView.image = [self UIImageFromCVMat:imageMat];
+    iplImage =[self CreateIplImageFromUIImage:image];
+    [self didCaptureIplImage:iplImage];
+
+    //image2 = [ self UIImageFromIplImage:iplImage];
+    //_imageView.image = image2;
+
+
+}
+
+- (IBAction)sliderValueChanged:(id)sender {
+    double rangeMIN = 0;
+    double rangeMAX = 180;
+    double step = 10;
     
-    _imageView.image = [self UIImageFromCVMat:imageMat];
+    _min = rangeMIN + _slider.value * (rangeMAX - rangeMIN - step);
+    _max = _min + step;
 }
 
 - (IBAction)pickImageFromGallery:(id)sender {
@@ -67,10 +120,193 @@ bool mouseSwiped;
     }
 }
 
+
+
+static BOOL _debug = NO;
+
+- (void)didCaptureIplImage:(IplImage *)iplImage
+{
+    //ipl image is in BGR format, it needs to be converted to RGB for display in UIImageView
+    IplImage *imgRGB = cvCreateImage(cvGetSize(iplImage), IPL_DEPTH_8U, 3);
+    cvCvtColor(iplImage, imgRGB, CV_BGR2RGB);
+    cv::Mat matRGB = cv::Mat(imgRGB);
+    
+    //ipl imaeg is also converted to HSV; hue is used to find certain color
+    IplImage *imgHSV = cvCreateImage(cvGetSize(iplImage), 8, 3);
+    cvCvtColor(iplImage, imgHSV, CV_BGR2HSV);
+    
+    IplImage *imgThreshed = cvCreateImage(cvGetSize(iplImage), 8, 1);
+    
+    //it is important to release all images EXCEPT the one that is going to be passed to
+    //the didFinishProcessingImage: method and displayed in the UIImageView
+    cvReleaseImage(&iplImage);
+    
+    //filter all pixels in defined range, everything in range will be white, everything else
+    //is going to be black
+    cvInRangeS(imgHSV, Scalar(_min, 100, 100), Scalar(_max, 255, 255), imgThreshed);
+    cvReleaseImage(&imgHSV);
+    
+    Mat matThreshed = Mat(imgThreshed);
+    
+    //smooths edges
+    cv::GaussianBlur(matThreshed,
+                     matThreshed,
+                     cv::Size(9, 9),
+                     2,
+                     2);
+    
+    //debug shows threshold image, otherwise the circles are detected in the
+    //threshold image and shown in the RGB image
+    if (_debug)
+    {
+        cvReleaseImage(&imgRGB);
+        [self UIImageFromIplImage:imgThreshed];
+        //[self didFinishProcessingImage:imgThreshed];
+    }
+    else
+    {
+        vector<Vec3f> circles;
+        
+        //get circles
+        HoughCircles(matThreshed,
+                     circles,
+                     CV_HOUGH_GRADIENT,
+                     2,
+                     matThreshed.rows / 4,
+                     150,
+                     75,
+                     10,
+                     150);
+    
+    NSLog(@"%lu",circles.size());
+        
+        for (size_t i = 0; i < circles.size(); i++)
+        {
+            cout << "Circle position x = " << (int)circles[i][0] << ", y = " << (int)circles[i][1] << ", radius = " << (int)circles[i][2] << "\n";
+            
+            cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            
+            int radius = cvRound(circles[i][2]);
+                    NSLog(@"%d",radius);
+
+            circle(matRGB, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+            circle(matRGB, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+        }
+        
+        //threshed image is not needed any more and needs to be released
+        cvReleaseImage(&imgThreshed);
+        
+        //imgRGB will be released once it is not needed, the didFinishProcessingImage:
+        //method will take care of that
+        //[self didFinishProcessingImage:imgRGB];
+    
+    _imageView.image = [self UIImageFromIplImage:imgRGB];
+    }
+}
+
+- (IplImage *)CreateIplImageFromUIImage:(UIImage *)image {
+    // Getting CGImage from UIImage
+    CGImageRef imageRef = image.CGImage;
+    
+    orientation = image.imageOrientation;
+    CGFloat cols,rows;
+    if(orientation == UIImageOrientationUp){
+        cols = image.size.height;
+        rows = image.size.width;
+    }
+    else{
+        cols = image.size.width;
+        rows = image.size.height;
+    }
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    // Creating temporal IplImage for drawing
+    IplImage *iplimage = cvCreateImage(
+                                       cvSize(cols,rows), IPL_DEPTH_8U, 4
+                                       );
+    // Creating CGContext for temporal IplImage
+    CGContextRef contextRef = CGBitmapContextCreate(
+                                                    iplimage->imageData, iplimage->width, iplimage->height,
+                                                    iplimage->depth, iplimage->widthStep,
+                                                    colorSpace, kCGImageAlphaPremultipliedLast|kCGBitmapByteOrderDefault
+                                                    );
+    // Drawing CGImage to CGContext
+    CGContextDrawImage(
+                       contextRef,
+                       CGRectMake(0, 0, image.size.width, image.size.height),
+                       imageRef
+                       );
+    CGContextRelease(contextRef);
+    CGColorSpaceRelease(colorSpace);
+    
+    // Creating result IplImage
+    IplImage *ret = cvCreateImage(cvGetSize(iplimage), IPL_DEPTH_8U, 3);
+    cvCvtColor(iplimage, ret, CV_RGBA2BGR);
+    cvReleaseImage(&iplimage);
+    
+    return ret;
+}
+
+// NOTE You should convert color mode as RGB before passing to this function
+- (UIImage *)UIImageFromIplImage:(IplImage *)image {
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    // Allocating the buffer for CGImage
+    NSData *data =
+    [NSData dataWithBytes:image->imageData length:image->imageSize];
+    CGDataProviderRef provider =
+    CGDataProviderCreateWithCFData((CFDataRef)data);
+    
+
+    
+    // Creating CGImage from chunk of IplImage
+    CGImageRef imageRef = CGImageCreate(
+                                        image->width, image->height,
+                                        image->depth, image->depth * image->nChannels, image->widthStep,
+                                        colorSpace, kCGImageAlphaNone|kCGBitmapByteOrderDefault,
+                                        provider, NULL, false, kCGRenderingIntentDefault
+                                        );
+    // Getting UIImage from CGImage
+    UIImage *ret = [UIImage imageWithCGImage:imageRef];
+    
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    return ret;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 - (cv::Mat)cvMatFromUIImage:(UIImage *)image{
     CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
-    CGFloat cols = image.size.width;
-    CGFloat rows = image.size.height;
+    
+    orientation = image.imageOrientation;
+    CGFloat cols,rows;
+    if(orientation == UIImageOrientationLeft || orientation == UIImageOrientationRight){
+        cols = image.size.height;
+        rows = image.size.width;
+    }
+    else{
+        cols = image.size.width;
+        rows = image.size.height;
+    }
+    
+    NSLog(@"width: @%f height: @%f", cols, rows);
+
+    medianBlur(imageMat,imageMat,5);
     
     cv::Mat cvMat(rows, cols, CV_8UC4);
     
@@ -117,7 +353,8 @@ bool mouseSwiped;
     
     
     // Getting UIImage from CGImage
-    UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    //UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef scale:1 orientation:orientation];
     CGImageRelease(imageRef);
     CGDataProviderRelease(provider);
     CGColorSpaceRelease(colorSpace);
@@ -156,79 +393,80 @@ bool mouseSwiped;
     return sqrt(pow(destination.x - source.x, 2) + pow(destination.y - source.y, 2)) <= (kSize/2);
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-    UITouch *touch = [touches anyObject];
-    
-    if ([touch view] == _tempImageView)
-    {
-        //getPosition
-        CGPoint touchPoint = [touch locationInView:_tempImageView];
-        
-        //conditioning if pixel is an edge
-        if ([self checkEdge:touchPoint kernelSize:5]) {
-            //            _statusClick.text = @"clickable";
-            lastPoint = touchPoint;
-        }
-        else{
-            //            _statusClick.text = @"no";
-        }
-    }
-    mouseSwiped = NO;}
-
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    
-    mouseSwiped = YES;
-    UITouch *touch = [touches anyObject];
-    CGPoint currentPoint = [touch locationInView:_tempImageView];
-    
-    if (lastPoint.x == -1) {
-        lastPoint = currentPoint;
-    }
-    else{
-        if ([self checkEdge:currentPoint kernelSize:5] and [self isNeighbor:currentPoint to:lastPoint kernelSize:15]){
-            UIGraphicsBeginImageContext(self.tempImageView.frame.size);
-            [self.tempImageView.image drawInRect:CGRectMake(0, 0, self.tempImageView.frame.size.width, self.tempImageView.frame.size.height)];
-            CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
-            CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y);
-            CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
-            CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 5 );
-            CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 255, 0, 0, 1.0);
-            CGContextSetBlendMode(UIGraphicsGetCurrentContext(),kCGBlendModeNormal);
-            
-            CGContextStrokePath(UIGraphicsGetCurrentContext());
-            self.tempImageView.image = UIGraphicsGetImageFromCurrentImageContext();
-            [self.tempImageView setAlpha:1];
-            UIGraphicsEndImageContext();
-            lastPoint = currentPoint;
-            [self checkMaxMinPoint:currentPoint];
-        }
-    }
-}
-
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    
-    if(!mouseSwiped) {
-        UIGraphicsBeginImageContext(self.tempImageView.frame.size);
-        [self.tempImageView.image drawInRect:CGRectMake(0, 0, self.tempImageView.frame.size.width, self.tempImageView.frame.size.height)];
-        CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
-        CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 5);
-        CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 255, 0, 0, 1);
-        CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
-        CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
-        CGContextStrokePath(UIGraphicsGetCurrentContext());
-        CGContextFlush(UIGraphicsGetCurrentContext());
-        self.tempImageView.image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-    }
-    
-    UIGraphicsBeginImageContext(self.imageView.frame.size);
-    [self.imageView.image drawInRect:CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height) blendMode:kCGBlendModeNormal alpha:1.0];
-    [self.tempImageView.image drawInRect:CGRectMake(0, 0, self.tempImageView.image.size.width, self.tempImageView.image.size.height) blendMode:kCGBlendModeNormal alpha:1];
-    self.imageView.image = UIGraphicsGetImageFromCurrentImageContext();
-    self.tempImageView.image = nil;
-    UIGraphicsEndImageContext();
-    
-    lastPoint = {-1,-1};
-}
+//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+//    UITouch *touch = [touches anyObject];
+//    
+//    if ([touch view] == _tempImageView)
+//    {
+//        //getPosition
+//        CGPoint touchPoint = [touch locationInView:_tempImageView];
+//        
+//        //conditioning if pixel is an edge
+//        if ([self checkEdge:touchPoint kernelSize:5]) {
+//            //            _statusClick.text = @"clickable";
+//            lastPoint = touchPoint;
+//        }
+//        else{
+//            //            _statusClick.text = @"no";
+//        }
+//    }
+//    mouseSwiped = NO;
+//}
+//
+//- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+//    
+//    mouseSwiped = YES;
+//    UITouch *touch = [touches anyObject];
+//    CGPoint currentPoint = [touch locationInView:_tempImageView];
+//    
+//    if (lastPoint.x == -1) {
+//        lastPoint = currentPoint;
+//    }
+//    else{
+//        if ([self checkEdge:currentPoint kernelSize:5] and [self isNeighbor:currentPoint to:lastPoint kernelSize:15]){
+//            UIGraphicsBeginImageContext(self.tempImageView.frame.size);
+//            [self.tempImageView.image drawInRect:CGRectMake(0, 0, self.tempImageView.frame.size.width, self.tempImageView.frame.size.height)];
+//            CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+//            CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y);
+//            CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
+//            CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 5 );
+//            CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 255, 0, 0, 1.0);
+//            CGContextSetBlendMode(UIGraphicsGetCurrentContext(),kCGBlendModeNormal);
+//            
+//            CGContextStrokePath(UIGraphicsGetCurrentContext());
+//            self.tempImageView.image = UIGraphicsGetImageFromCurrentImageContext();
+//            [self.tempImageView setAlpha:1];
+//            UIGraphicsEndImageContext();
+//            lastPoint = currentPoint;
+//            [self checkMaxMinPoint:currentPoint];
+//        }
+//    }
+//}
+//
+//- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+//    
+//    if(!mouseSwiped) {
+//        UIGraphicsBeginImageContext(self.tempImageView.frame.size);
+//        [self.tempImageView.image drawInRect:CGRectMake(0, 0, self.tempImageView.frame.size.width, self.tempImageView.frame.size.height)];
+//        CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
+//        CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 5);
+//        CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 255, 0, 0, 1);
+//        CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+//        CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+//        CGContextStrokePath(UIGraphicsGetCurrentContext());
+//        CGContextFlush(UIGraphicsGetCurrentContext());
+//        self.tempImageView.image = UIGraphicsGetImageFromCurrentImageContext();
+//        UIGraphicsEndImageContext();
+//    }
+//    
+//    UIGraphicsBeginImageContext(self.imageView.frame.size);
+//    [self.imageView.image drawInRect:CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height) blendMode:kCGBlendModeNormal alpha:1.0];
+//    [self.tempImageView.image drawInRect:CGRectMake(0, 0, self.tempImageView.image.size.width, self.tempImageView.image.size.height) blendMode:kCGBlendModeNormal alpha:1];
+//    self.imageView.image = UIGraphicsGetImageFromCurrentImageContext();
+//    self.tempImageView.image = nil;
+//    UIGraphicsEndImageContext();
+//    
+//    lastPoint = {-1,-1};
+//}
 
 @end
